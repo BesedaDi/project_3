d6df9d501db6b4847a7aff26a018abac45f9c65c
BesedaDi <dianabesedina223@gmail.com>
2019-10-21 21:00:22 +0700

    finish project

---
+++ Project3.py
@@ -0,0 +1,12 @@
+# Система распознования пароля, введеного пользователем
+password = int(input())
+s = 0
+while s < password:
+    s = s + 1
+a=[password, s]
+for i in a:
+    print(i)
+if s==password:
+    print('Пароль подобран верно')
+else:
+    print('Пароль подобран не верно, система ошиблась!')
